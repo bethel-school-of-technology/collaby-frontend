819d59c15aebcbf1a2764d3efd55b3fbd366cfa8
BRANDENKENN <brandenkenn@gmail.com>
2020-01-21 22:09:19 -0500

    updated routing for profile component and started login function

--- Collaby/src/app/login/login.component.ts
+++ Collaby/src/app/login/login.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  password = (<HTMLInputElement>document.getElementById("password")).value
+
+  lengthCheck() {
+
+  }
+
   constructor() { }
 
   ngOnInit() {
